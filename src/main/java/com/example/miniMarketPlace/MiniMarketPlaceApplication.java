b30033d07815996f52512e9e95fa80b89b5204d9
dyhurtado_meli <dyhurtado@mercadolibre.com>
2026-03-14 09:40:50 -0300

    Bootstrap Spring Boot project

---
+++ src/main/java/com/example/miniMarketPlace/MiniMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniMarketPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMarketPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMarketPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniMarketPlace/MiniMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniMarketPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
